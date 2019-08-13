4fa31cc4f41aa79fd6328557eb1a07cec114afb5
David Piper <black.god1930@gmail.com>
2019-08-13 08:24:43 -0400

    Updates

---
+++ precache-manifest.de939824f680a5a6c0f9b4b4e93c2b7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d44b4a98a502316834a73f5698ce4da",
+    "url": "/http--01-starting-setup/index.html"
+  },
+  {
+    "revision": "4d0bb0cbd40d84433c88",
+    "url": "/http--01-starting-setup/static/css/main.790f26b9.chunk.css"
+  },
+  {
+    "revision": "56faf9d694734cecd7f4",
+    "url": "/http--01-starting-setup/static/js/2.6202b3e1.chunk.js"
+  },
+  {
+    "revision": "4d0bb0cbd40d84433c88",
+    "url": "/http--01-starting-setup/static/js/main.b3dec9f6.chunk.js"
+  },
+  {
+    "revision": "586f65158764bc4cf07b",
+    "url": "/http--01-starting-setup/static/js/runtime~main.fdd1cdd4.js"
+  }
+]);
